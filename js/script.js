a533604e1f54f0e747ffd76dc031c8fbfb4eeaaf
kate <kate.klimanovski@gmail.com>
2019-03-29 15:49:12 +0300

    script

--- js/script.js
+++ js/script.js
@@ -16,31 +16,3 @@ if (searchOpen) {
   });
 };
 
-// борода
-
-  var link = document.querySelector(".login-link");
-  
-  var popup = document.querySelector(".modal-login");
-  var close = popup.querySelector(".modal-close");
-  
-  var login = popup.querySelector("[name=login]");
-  
-  link.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.add("modal-show");
-    login.focus();
-  });
-  
-  close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal-show");
-  });
-
-
-
-
-
-
-
-
-
